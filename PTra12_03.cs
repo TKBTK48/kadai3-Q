e3e1efeda2a956cef23e9f632b959d3ed0d283dc
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 22:52:10 +0900

    Create PTra12_03.cs

---
+++ PTra12_03.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * PTra12_03.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+using common;
+
+public class PTra12_03
+{
+    /*
+  	 * TODO common内に、Characterクラスを継承した、Heroクラスを作成してください
+  	 * 	●HP, POWER, ENDURANCEはそれぞれ、（25, 10, 7）
+  	 */
+
+    public static void Main(string[] args)
+    {
+        // TODO Heroクラスをインスンタンス化してください
+
+        // TODO Heroインスタンスのnameフィールドに 勇者 を設定して下さい
+
+        // TODO Heroインスタンスのキャラクター情報を表示してください
+
+    }
+}
