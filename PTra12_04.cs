397a7503a2e2d58c09fe48de5ae01521ae3f8437
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 22:53:26 +0900

    Create PTra12_04.cs

---
+++ PTra12_04.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * PTra12_04.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+using common;
+
+public class PTra12_04
+{
+    /* 
+     * TODO common内にCharacterクラスを継承した、Slimeクラスを作成してください
+  	 * ●HP, POWER, ENDURANCEはそれぞれ、（10, 5, 2）
+  	 */
+
+    public static void Main(string[] args)
+    {
+        // TODO Slimeクラスをインスンタンス化してください
+
+        // TODO Slimeインスタンスのnameフィールドに スライム を設定して下さい
+
+        // TODO Slimeインスタンスのキャラクター情報を表示してください
+        
+    }
+}
