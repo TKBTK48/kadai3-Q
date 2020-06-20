c4c49de99a15ed10acaa3adb5e1871f98197cfaf
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 22:50:27 +0900

    Create PTra12_01.cs

---
+++ PTra12_01.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * PTra12_01.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+using common;
+
+//※ この問題はcommonのCatクラスを完成させてから実施してください。
+public class PTra12_01
+{
+    public static void Main(string[] args)
+    {
+        // TODO commonのCatクラスをインスンタンス化してください
+
+        // TODO Cat型インスタンスのnameフィールドに タマ を代入してください
+
+        // TODO Cat型インスタンスのRunメソッドを実行してください
+        // ※ タマは走りました。が表示されれば正解
+
+        // TODO Cat型インスタンスのEatメソッドを実行してください
+        // ※ タマは食事をしました。が表示されれば正解
+
+    }
+}
