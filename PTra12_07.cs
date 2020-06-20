19d497b0d9dc835843f707fc13750326a6d30c17
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 22:55:55 +0900

    Create PTra12_07.cs

---
+++ PTra12_07.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * PTra12_07.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+using common;
+
+public class PTra12_07
+{
+    /*
+  	 * 作成した、SuperHero、Slimeクラスを使用します
+  	 */
+    public static void Main(string[] args)
+    {
+        // TODO SuperHeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者(装備あり)", "スライム"を設定してください
+
+        // TODO Itemクラスのインスタンスを作成し、(ロングソード", 20）をコンストラクタの引数にしてください
+
+        // TODO SuperHeroインスンタンスのitemフィールドに作成したitemインスタンスをセットしてください
+
+        /*
+    	 * TODO SuperHeroとSlimeを、どちらかが体力０になるまで戦わせます
+    	 *
+    	 * 	●SuperHeroの攻撃 -> ダメージ判定 -> Slimeの攻撃 -> ダメージ判定
+    	 * 上記を繰り返し行います
+    	 *
+    	 * 戦闘が終了したら、勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
+    	 */
+        
+    }
+}
