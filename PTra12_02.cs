c1245004ec2bcbfa55f7e9e22c500c2338e517ca
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 22:51:26 +0900

    Create PTra12_02.cs

---
+++ PTra12_02.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * PTra12_02.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+using common;
+
+//※ この問題はcommonのDogクラスを完成させてから実施してください。
+public class PTra12_02
+{
+    public static void Main(string[] args)
+    {
+        // TODO commonのDogクラスをインスンタンス化してください
+
+        // TODO Dog型インスタンスのnameフィールドに ポチ を代入してください
+
+        // TODO Dog型インスタンスのRunメソッドを実行してください
+        // ※ ポチは走りました。が表示されれば正解
+
+        // TODO Dog型インスタンスのEatメソッドを実行してください
+        // ※ ポチはドッグフードを食べました。が表示されれば正解
+
+    }
+}
