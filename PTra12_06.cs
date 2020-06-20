1c3477910d1e26e7a113332404308ca026628780
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 22:55:09 +0900

    Create PTra12_06.cs

---
+++ PTra12_06.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * PTra12_06.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+using common;
+
+public class PTra12_06
+{
+    /*
+  	 * TODO commonにHeroクラスを継承した、SuperHeroクラスを作成してください
+  	 * 	●HP, POWER, ENDURANCEはHeroと同じでそれぞれ、（25, 10, 7）
+  	 *
+  	 * フィールド
+  	 * 		item	:	Item
+  	 * メソッド
+  	 * 		Attackメソッドをオーバーライドし、
+  	 * 		powerフィールドの値に加えて、itemフィールドのadditionalDamage分を加算した値を返す
+  	 *
+  	 * 		itemフィールドのアクセサ
+  	 */
+    public static void Main(string[] args)
+    {
+        // TODO SuperHeroインスタンスを作成し、名前に"勇者(装備あり)"を設定してください
+
+        // TODO Itemクラスのインスタンスを作成し、("こんぼう", 5）をコンストラクタの引数にしてください
+
+        // TODO SuperHeroインスンタンスのitemフィールドに作成したitemインスタンスをセットしてください
+
+        // TODO SuperHeroインスンタンスのAttackメソッドを呼び出して、戻り値を出力してください
+        // ※ 15と出力されれば正解
+        
+    }
+}
