6fc5a8274872251e3aa5774a8da4dd2e29018df1
TKBTK48 <66378115+TKBTK48@users.noreply.github.com>
2020-06-20 22:54:14 +0900

    Create PTra12_05.cs

---
+++ PTra12_05.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * PTra12_05.cs
+ *   作成	Rhizome
+ *------------------------------------------------------------
+ * Copyright(c) Rhizome Inc. All Rights Reserved.
+ */
+
+using System;
+using common;
+
+public class PTra12_05
+{
+    /*
+  	 * 作成した、Hero、Slimeクラスを使用します
+  	 */
+    public static void Main(string[] args)
+    {
+        // TODO HeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者", "スライム"を設定してください
+
+        /*
+    	 * TODO HeroとSlimeを、どちらかが体力０になるまで戦わせます
+    	 *
+    	 * 	●Heroの攻撃 -> ダメージ判定 -> Slimeの攻撃 -> ダメージ判定
+    	 * 上記を繰り返し行います
+    	 *
+    	 * 戦いが終了したら、勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
+    	 */
+        
+    }
+}
